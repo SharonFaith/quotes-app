861ce1b886d3e8dfc3bd79c20493fdf43947e63e
SharonFaith <sharonfaith15@gmail.com>
2020-09-26 12:44:22 +0300

    create quotes array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+   
+  quotes: Quote[] = [
+    // id, quoteBody, publisher, datePublished, author
+    new Quote(1, 'Push yourself, no one else will', 'Patrick', new Date(2019/9/24), 'Zawadi'),
+    new Quote(2, 'Limits exist only in the mind', 'Kate', new Date(2019/9/25), 'Joy'),
+    new Quote(3, 'Spread the love you want to receive', 'Amani', new Date(2019/9/26), 'Sally'),
+    new Quote(4, 'Normal is boring', 'Daisy', new Date(2019/9/27 ), 'Danielle'),
+    new Quote(5, 'Do not dim your light', 'Rob', new Date(2019/9/28), 'Claire')
+  ]
 
   constructor() { }
 
