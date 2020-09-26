52528c039bd248d0457ec57cda3545d85b7cf2d5
SharonFaith <sharonfaith15@gmail.com>
2020-09-26 12:13:50 +0300

    add constructor of quote class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,4 @@
 export class Quote {
+    constructor(public id: number, public quoteBody: string, public publisher: string, public datePublished: Date, public author: string) {}
 }
+// id, quoteBody, publisher, datePublished, author
