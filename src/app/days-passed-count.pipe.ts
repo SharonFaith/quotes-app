d8249f16f2cc6932c5e6fcec00185b231ad6f5ca
SharonFaith <sharonfaith15@gmail.com>
2020-09-27 20:55:55 +0300

    define pipe and apply it in quote details section

--- src/app/days-passed-count.pipe.ts
+++ src/app/days-passed-count.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DaysPassedCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(todayNoTime - value) //returns value in milliseconds
+    const secondsInDay = 86400
+    var dateDifferenceSeconds = dateDifference*0.001 //CONVERTS MILLISECONDS TO SECONDS
+    var dateCounter = dateDifferenceSeconds/secondsInDay //get days
+
+    if(dateCounter >= 1 && value < todayNoTime) {
+      return dateCounter;
+    } else {
+      return 0
+    }
   }
 
 }
