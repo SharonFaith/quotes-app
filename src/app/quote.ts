476a766246f56ec3718609fa4a0993748855142c
SharonFaith <sharonfaith15@gmail.com>
2020-09-27 20:45:28 +0300

    toggle quote details

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,7 @@
 export class Quote {
-    constructor(public id: number, public quoteBody: string, public publisher: string, public datePublished: Date, public author: string) {}
+    showDescription:boolean
+    constructor(public id: number, public quoteBody: string, public publisher: string, public datePublished: Date, public author: string) {
+        this.showDescription = false
+    }
 }
 // id, quoteBody, publisher, datePublished, author
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,13 +10,15 @@ export class QuoteComponent implements OnInit {
    
   quotes: Quote[] = [
     // id, quoteBody, publisher, datePublished, author
-    new Quote(1, 'Push yourself, no one else will', 'Patrick', new Date(2019/9/24), 'Zawadi'),
-    new Quote(2, 'Limits exist only in the mind', 'Kate', new Date(2019/9/25), 'Joy'),
-    new Quote(3, 'Spread the love you want to receive', 'Amani', new Date(2019/9/26), 'Sally'),
-    new Quote(4, 'Normal is boring', 'Daisy', new Date(2019/9/27 ), 'Danielle'),
-    new Quote(5, 'Do not dim your light', 'Rob', new Date(2019/9/28), 'Claire')
+    new Quote(1, 'Push yourself, no one else will', 'Patrick', new Date(2019, 6, 24), 'Zawadi'),
+    new Quote(2, 'Limits exist only in the mind', 'Kate', new Date(2019, 6,25), 'Joy'),
+    new Quote(3, 'Spread the love you want to receive', 'Amani', new Date(2019, 6,26), 'Sally'),
+    new Quote(4, 'Normal is boring', 'Daisy', new Date(2019, 6, 27), 'Danielle'),
+    new Quote(5, 'Do not dim your light', 'Rob', new Date(2019, 6, 28), 'Claire')
   ]
-
+   toggleDetails(index) {
+     this.quotes[index].showDescription = !this.quotes[index].showDescription;
+   }
   constructor() { }
 
   ngOnInit(): void {
