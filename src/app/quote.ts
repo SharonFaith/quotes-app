be6ace0880ebc8a4dcb93cc61312c6330adf371b
SharonFaith <sharonfaith15@gmail.com>
2020-09-28 03:54:46 +0300

    add upvotes and downvotes to quote class, add buttons

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription:boolean
-    constructor(public id: number, public quoteBody: string, public publisher: string, public datePublished: Date, public author: string) {
+    constructor(public id: number, public quoteBody: string, public publisher: string, public datePublished: Date, public author: string, public upVotes: number, public downVotes: number) {
         this.showDescription = false
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,11 +10,11 @@ export class QuoteComponent implements OnInit {
    
   quotes: Quote[] = [
     // id, quoteBody, publisher, datePublished, author
-    new Quote(1, 'Push yourself, no one else will', 'Patrick', new Date(2019, 6, 24), 'Zawadi'),
-    new Quote(2, 'Limits exist only in the mind', 'Kate', new Date(2019, 6,25), 'Joy'),
-    new Quote(3, 'Spread the love you want to receive', 'Amani', new Date(2019, 6,26), 'Sally'),
-    new Quote(4, 'Normal is boring', 'Daisy', new Date(2019, 6, 27), 'Danielle'),
-    new Quote(5, 'Do not dim your light', 'Rob', new Date(2019, 6, 28), 'Claire')
+    new Quote(1, 'Push yourself, no one else will', 'Patrick', new Date(2019, 6, 24), 'Zawadi', 0, 0),
+    new Quote(2, 'Limits exist only in the mind', 'Kate', new Date(2019, 6,25), 'Joy', 0, 0),
+    new Quote(3, 'Spread the love you want to receive', 'Amani', new Date(2019, 6,26), 'Sally', 0, 0),
+    new Quote(4, 'Normal is boring', 'Daisy', new Date(2019, 6, 27), 'Danielle', 0, 0),
+    new Quote(5, 'Do not dim your light', 'Rob', new Date(2019, 6, 28), 'Claire', 0, 0)
   ]
    toggleDetails(index) {
      this.quotes[index].showDescription = !this.quotes[index].showDescription;
@@ -32,6 +32,8 @@ export class QuoteComponent implements OnInit {
       let quoteLength = this.quotes.length;
       quote.id = quoteLength + 1;
       quote.datePublished = new Date(quote.datePublished)
+      quote.upVotes = 0
+      quote.downVotes = 0
       this.quotes.unshift(quote)
     }
    
